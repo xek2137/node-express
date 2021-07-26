1bef27b2b762e052d2fa4f8c05e7bbabdde543e0
Adrian Piech <adrianpiech.git@gmail.com>
2021-07-26 09:17:04 +0200

    Add html views/

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('<h1>Hi there!</h1>')
+})
+
+app.get('/about', (req, res) => {
+  res.send('<h1>About</h1>')
+})
+app.get('/contact', (req, res) => {
+  res.send('<h1>Contact</h1>')
+})
+app.get('/info', (req, res) => {
+  res.send('<h1>Info</h1>')
+})
+app.get('/history', (req, res) => {
+  res.send('<h1>History</h1>')
+})
+
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
