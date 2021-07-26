7cb21e1dfa0e26271cf6d562dd0b306e3001fca3
Adrian Piech <adrianpiech.git@gmail.com>
2021-07-26 14:57:58 +0200

    Add middlewares & refactor code

--- server.js
+++ server.js
@@ -3,30 +3,39 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '/public')));
+
+
 app.use((req, res, next) => {
   res.show = name => {
-    res.sendFile(path.join(__dirname, `/views/${name}show`));
+    res.sendFile(path.join(__dirname, `views/${name}`));
   };
   next();
+});
+app.use('/user/:id', (req, res, next) => {
+  res.show('forbidden.html');
+  next()
 })
 
-app.get('/', (req, res) => {
-  res.show('index.html');
+app.get(['/', '/home'], (req, res) => {
+  res.show('home.html');
 });
-
 app.get('/about', (req, res) => {
   res.show('about.html');
 });
 app.get('/contact', (req, res) => {
   res.show('contact.html');
 });
-app.get('/info', (req, res) => {
-  res.show('info.html');
+app.get('/user', (req, res) => {
+  res.show('forbidden.html')
 });
 app.get('/history', (req, res) => {
   res.show('history.html');
 });
 
+app.use((req, res) => {
+  res.status(404).show('404.html');
+});
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
