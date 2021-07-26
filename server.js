da54f0bc51890177386f0e5b5c96140f75cdec25
Adrian Piech <adrianpiech.git@gmail.com>
2021-07-26 19:59:22 +0200

    Add handlebars

--- server.js
+++ server.js
@@ -1,41 +1,35 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+app.engine('hbs', hbs({extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main'}));
+app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-
-app.use((req, res, next) => {
-  res.show = name => {
-    res.sendFile(path.join(__dirname, `views/${name}`));
-  };
-  next();
-});
 app.use('/user/:id', (req, res, next) => {
-  res.show('forbidden.html');
+  res.render('forbidden', {layout: false});
   next()
 })
 
-app.get(['/', '/home'], (req, res) => {
-  res.show('home.html');
+app.get(['/hello/:name', '/hello'], (req, res) => {
+  res.render('hello', {layout: false, name: req.params.name});
+});
+app.get(['/', '/home/'], (req, res) => {
+  res.render('home', {layout: false});
 });
 app.get('/about', (req, res) => {
-  res.show('about.html');
-});
-app.get('/contact', (req, res) => {
-  res.show('contact.html');
+  res.render('about', {layout: false});
 });
 app.get('/user', (req, res) => {
-  res.show('forbidden.html')
-});
-app.get('/history', (req, res) => {
-  res.show('history.html');
+  res.render('forbidden', {layout: false})
 });
 
 app.use((req, res) => {
-  res.status(404).show('404.html');
+  res.status(404).render('404', {layout: false});
 });
+
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
